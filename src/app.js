afa2e9abf838a191fac45df3c731c50eb56c7607
Avinash Kumar <avinash.k95347@gmail.com>
2024-04-02 00:35:10 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import {dbConnect} from "./connections/dbConnect.js";
-import { app } from "./index.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 
-dbConnect().then(() => {
-    app.listen(process.env.APP_PORT || 5000, () =>
-      console.log("App is running on port", process.env.APP_PORT)
-    );
-  })
-  .catch((err) => console.log("connection failed", err?.message));
- 
+const app = express();
+
+app.use(express.json({limit:'16kb'}));
+app.use(express.urlencoded({ extended: true, limit:'16kb' }));
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(cors({origin:process.env.CORS_ORIGIN}));
+
+
+// Routes import
+import {reportRouter} from './routes/report.routes.js';
+
+
+app.get('/',(req,res)=>{
+    res.send('hello world!!')
+})
+// Routes
+app.use('/report', reportRouter);
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import {dbConnect} from "./connections/dbConnect.js";
+import { app } from "./app.js";
 
 
-const app = express();
-
-app.use(express.json({limit:'16kb'}));
-app.use(express.urlencoded({ extended: true, limit:'16kb' }));
-app.use(cookieParser());
-app.use(express.static('public'));
-app.use(cors({origin:process.env.CORS_ORIGIN}));
-
-
-// Routes import
-import {reportRouter} from './routes/report.routes.js';
-
-
-app.get('/',(req,res)=>{
-    res.send('hello world!!')
-})
-// Routes
-app.use('/report', reportRouter);
-
-export {app};
+dbConnect().then(() => {
+    app.listen(process.env.APP_PORT || 5000, () =>
+      console.log("App is running on port", process.env.APP_PORT)
+    );
+  })
+  .catch((err) => console.log("connection failed", err?.message));
+ 
